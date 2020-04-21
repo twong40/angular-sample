13fd81d0b833c5ac724724c905d9d46f9c16d7e6
thomaswong1000@gmail.com <twong40@ucsc.edu>
2020-04-20 22:33:04 -0700

    added custom interface, pipe, styling, and data binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
-
+import { FormsModule } from "@angular/forms";
 import { AppComponent } from "./app.component";
 import { ProductListComponent } from "./products/product-list.component";
+import { ConvertToSpacesPipe } from "./shared/convert-to-spaces.pipe";
 
 @NgModule({
-  declarations: [AppComponent, ProductListComponent],
-  imports: [BrowserModule],
+  declarations: [AppComponent, ProductListComponent, ConvertToSpacesPipe],
+  imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
